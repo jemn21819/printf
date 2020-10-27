959f92cfe617626d5bf955613e5efd3a8c6f9ce3
Jaime Martinez <jemn1jemn@gmail.com>
2020-10-27 21:28:00 +0000

    remmoving wrong file

--- get_func.c
+++ get_func.c
@@ -16,6 +16,11 @@ int (*get_func(const char *format))(va_list)
 		{"d", print_int},
 		{"%", print_percent},
 		{"b", print_bin},
+		{"u", print_unsign},
+		{"o", print_octal},
+		{"x", print_x},
+		{"X", print_X},
+		{"R", print_rot13},
 		{NULL, NULL}
 	};
 
