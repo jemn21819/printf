0a742f8e56e0c320333fa7ace3074f5b5673353c
Jaime Martinez <jemn1jemn@gmail.com>
2020-10-28 03:03:29 +0000

    fix file

--- _putchar.c
+++ _putchar.c
@@ -8,18 +8,5 @@
  */
 int _putchar(char c)
 {
-	char buffer[1024];
-	int i;
-
-	if (c == -1 || i >= 1024)
-	{
-		write(1, &buffer, i);
-		i = 0;
-	}
-	else
-	{
-		buffer[i] = c;
-		i++;
-	}
-	return (1);
+	return (write(1, &c, 1));
 }
